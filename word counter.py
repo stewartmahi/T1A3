3e4839bfb15d2c466bfb7eea5fad22cbf8e9c799
stewartmahi <114791982+stewartmahi@users.noreply.github.com>
2023-05-09 20:14:57 +1000

    print number of words

--- word counter.py
+++ word counter.py
@@ -14,3 +14,13 @@ try:
 except FileNotFoundError:
     print(f"Error: file '{file_path}' not found")
     sys.exit(1)
+
+# Count the number of words, characters, and lines in the file
+num_words = len(contents.split())
+num_chars = len(contents)
+num_lines = len(contents.split("\n"))
+
+# Output the results to the console
+print(f"Words: {num_words}")
+print(f"Characters: {num_chars}")
+print(f"Lines: {num_lines}")
