af9dfd20c965b94e94fbe7101dbda081796a3ca3
stewartmahi <114791982+stewartmahi@users.noreply.github.com>
2023-05-09 20:13:59 +1000

    read contents of file

--- word counter.py
+++ word counter.py
@@ -6,3 +6,11 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 file_path = sys.argv[1]
+
+# Read the contents of the file
+try:
+    with open(file_path, "r") as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Error: file '{file_path}' not found")
+    sys.exit(1)
