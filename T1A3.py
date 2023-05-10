ef6ddc34ebd7529b8964fd2a2f25b9aeb23be1b8
stewartmahi <114791982+stewartmahi@users.noreply.github.com>
2023-05-10 20:59:05 +1000

    Adding shuffle deck function

--- T1A3.py
+++ T1A3.py
@@ -27,3 +27,17 @@ def build_deck():
     return deck
 
 # for the build_deck() we create a nested for loop to run through all the values and colors, and use the append function to add this values, to the deck which is a list of strings. We then run another for loop for the wilds and this allows us to add the 4 wild of each type to the deck as well.
+
+"""We want to be able to shuffle the deck as well, as when we generate the deck, it will be in order. Parameters: deck (list), Return shuffled_deck (list)"""
+
+def shuffle_deck(deck):
+
+    for card_position in range(len(deck)):
+        random_position = random.randint(0,107)
+        deck[card_position], deck[random_position] = deck[random_position], deck[card_position]
+    
+    return deck
+
+#this function allows us to take two positions, the card position and the random position, and allows us to switch each card in the list, with another card in a random position in the list 
+
+"""Next we need a function to allow the player to draw cards at the beginning, throughout as well when the player cannot play their turn, parameter is int (number of cards) and it returns cards drawn (list)"""
